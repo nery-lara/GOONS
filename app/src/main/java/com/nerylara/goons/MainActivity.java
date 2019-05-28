8cca9fe8123413df8add485c5303f36b670fa21b
avSB <avasquezramirez@ucsb.edu>
2019-05-27 18:49:37 -0700

    Implemented Navigation Buttons using Fragments & Container

--- app/src/main/java/com/nerylara/goons/MainActivity.java
+++ app/src/main/java/com/nerylara/goons/MainActivity.java
@@ -2,41 +2,49 @@ package com.nerylara.goons;
 
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
-        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navView.setOnNavigationItemSelectedListener(navListener);
+
+        // Start Profile Screen
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container,new ProfileFragment())
+                .commit();
     }
 
+    private  BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener(){
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item){
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()){
+                        case R.id.navProfile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                        case R.id.navWorld:
+                            selectedFragment = new WorldFragment();
+                            break;
+                        case R.id.navSquads:
+                            selectedFragment = new SquadsFragment();
+                            break;
+                    }
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment_container,selectedFragment)
+                            .commit();
+                    return true;
+                }
+            };
 }
