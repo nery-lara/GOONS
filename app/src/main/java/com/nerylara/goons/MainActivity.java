9b95b78eb2a86cc7bb02256299abbde9bfb24985
Nery Lara <nerylara4@gmail.com>
2019-06-10 09:16:29 -0700

    active, game, joinSquad, location, login, profile, signUp, squad functions implemented

--- app/src/main/java/com/nerylara/goons/MainActivity.java
+++ app/src/main/java/com/nerylara/goons/MainActivity.java
@@ -1,6 +1,11 @@
 package com.nerylara.goons;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
@@ -8,6 +13,7 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
     private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -36,7 +42,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.message);
+
+
+        /* request permissions */
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            //do nothing
+        }else{
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+        }
+
+
+
+
+
     }
 
 }
